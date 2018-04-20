2cd5a6f0195992f9d93279bd76225079de71cfe9
Ryan Dwyer <ryan@Ryans-MacBook-Pro-2.local>
2018-04-20 09:33:10 -0400

    wip

--- angular_flask/static/js/controllers/mashController.js
+++ angular_flask/static/js/controllers/mashController.js
@@ -18,23 +18,25 @@
             grainAbsorption: 0.1,
             thermodynamicConstant: 0.2,
             mashTechnique: 0,
-            equipmentProfile: {
-                grainAbsorptionFactor: 0.1,
-                boilingTemp: 212,
-                mashThickness: 1.25,
-                lauterDeadspace: 0,
-            },
-            output: {
-                mashWaterVolume: 0,
-                strikeWaterTemp: 0,
-                spargeWaterVolume: 0,
-                combinedRunningsVolume: 0
-            }
+        };
+
+        this.equipmentProfile = {
+            grainAbsorptionFactor: 0.1,
+            boilingTemp: 212,
+            lauterDeadspace: 0,
+        };
+
+        this.output = {
+            mashWaterVolume: 0,
+            strikeWaterTemp: 0,
+            spargeWaterVolume: 0,
+            combinedRunningsVolume: 0
         };
 
         this.calculate = calculate;
         this.reset = reset;
         this._strikeWaterTemp = _strikeWaterTemp;
+        this.prettify = prettify;
 
         function calculate() {
             // quarts of water
@@ -46,10 +48,10 @@
             // quarts of water pre boil
             var combinedRunningsVolume = mashWaterVolume + spargeWaterVolume - ( this.data.grainWeight * this.data.grainAbsorption );
 
-            this.data.output.mashWaterVolume = mashWaterVolume;
-            this.data.output.strikeWaterTemp = strikeWaterTemp;
-            this.data.spargeWaterVolume = spargeWaterVolume;
-            this.data.combinedRunningsVolume = combinedRunningsVolume;
+            this.output.mashWaterVolume = mashWaterVolume;
+            this.output.strikeWaterTemp = strikeWaterTemp;
+            this.output.spargeWaterVolume = spargeWaterVolume;
+            this.output.combinedRunningsVolume = combinedRunningsVolume;
         }
 
         function _strikeWaterTemp(mashThickness, mashTemp, grainTemp) {
@@ -70,13 +72,24 @@
                 grainAbsorption: 0.1,
                 thermodynamicConstant: 0.2,
                 mashTechnique: 0,
-                equipmentProfile: {
-                    grainAbsorptionFactor: 0.1,
-                    boilingTemp: 212,
-                    mashThickness: 1.25,
-                    lauterDeadspace: 0,
-                }
+            };
+            this.equipmentProfile = {
+                grainAbsorptionFactor: 0.1,
+                boilingTemp: 212,
+                mashThickness: 1.25,
+                lauterDeadspace: 0,
+            };
+            this.output = {
+                mashWaterVolume: 0,
+                strikeWaterTemp: 0,
+                spargeWaterVolume: 0,
+                combinedRunningsVolume: 0
             };
         }
+
+        function prettify(text) {
+            return text.split(/(?=[A-Z])/).join(" ");
+        }
+
     }
 })();
